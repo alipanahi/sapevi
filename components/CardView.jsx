304b36cb6f78d239edbf89db72e062617bb78d61
sajadoncode <sajadoncode@gmail.com>
2022-12-08 12:49:34 +0100

    create card view component

---
+++ components/CardView.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Image from "next/image";
+
+const CardView = (props) => {
+  console.log("from tags", props.tag);
+  return (
+    <div className="col-3 pt-3">
+      <div class="card mb-3 border-0 shadow-sm">
+        <Image
+          src={props.img}
+          className="card-img-top"
+          alt="computer"
+          width={100}
+          height={200}
+        />
+        <div class="card-body">
+          <h5 class="card-title">{props.title}</h5>
+          <p class="card-text text-secondary">{props.desc}</p>
+          <p class="card-text">
+            <small class="text-muted"># {props.tag}</small>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardView;
