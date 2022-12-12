76c362892e6b428d9c4589c601bbc41188c5d53c
alihpanahi <p.alihussain@gmail.com>
2022-12-12 15:49:58 +0100

    homepage-profile-settings

--- database/seeders/20221206155127-demo-category.js
+++ database/seeders/20221206155127-demo-category.js
@@ -5,7 +5,7 @@ module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Categories', [{
       title: 'General Knowledge',
-      imgUrl: 'https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
+      imgUrl: 'https://images.unsplash.com/photo-1593061231114-1798846fd643?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas facere dignissimos nihil quibusdam illum id assumenda animi a suscipit minus dolorem error molestiae, quasi, autem vitae aspernatur odio magnam facilis.",
       number_of_question: 5,
       code: 9,
@@ -15,7 +15,7 @@ module.exports = {
     },
     {
       title: 'Entertainment: Books',
-      imgUrl: 'https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
+      imgUrl: 'https://images.unsplash.com/photo-1513001900722-370f803f498d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas facere dignissimos nihil quibusdam illum id assumenda animi a suscipit minus dolorem error molestiae, quasi, autem vitae aspernatur odio magnam facilis.",
       number_of_question: 5,
       code: 10,
@@ -25,7 +25,7 @@ module.exports = {
     },
     {
       title: 'Entertainment: Film',
-      imgUrl: 'https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
+      imgUrl: 'https://images.unsplash.com/photo-1478720568477-152d9b164e26?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas facere dignissimos nihil quibusdam illum id assumenda animi a suscipit minus dolorem error molestiae, quasi, autem vitae aspernatur odio magnam facilis.",
       number_of_question: 5,
       code: 11,
@@ -35,7 +35,7 @@ module.exports = {
     },
     {
       title: 'Entertainment: Music',
-      imgUrl: 'https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
+      imgUrl: 'https://images.unsplash.com/photo-1511379938547-c1f69419868d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas facere dignissimos nihil quibusdam illum id assumenda animi a suscipit minus dolorem error molestiae, quasi, autem vitae aspernatur odio magnam facilis.",
       number_of_question: 5,
       code: 12,
@@ -45,7 +45,7 @@ module.exports = {
     },
     {
       title: 'Entertainment: Video Games',
-      imgUrl: 'https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
+      imgUrl: 'https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas facere dignissimos nihil quibusdam illum id assumenda animi a suscipit minus dolorem error molestiae, quasi, autem vitae aspernatur odio magnam facilis.",
       number_of_question: 5,
       code: 15,
@@ -65,7 +65,7 @@ module.exports = {
     },
     {
       title: 'Science: Mathematics',
-      imgUrl: 'https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
+      imgUrl: 'https://images.unsplash.com/photo-1509228468518-180dd4864904?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas facere dignissimos nihil quibusdam illum id assumenda animi a suscipit minus dolorem error molestiae, quasi, autem vitae aspernatur odio magnam facilis.",
       number_of_question: 5,
       code: 19,
@@ -75,7 +75,7 @@ module.exports = {
     },
     {
       title: 'Sports',
-      imgUrl: 'https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80',
+      imgUrl: 'https://images.unsplash.com/photo-1579952363873-27f3bade9f55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80',
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas facere dignissimos nihil quibusdam illum id assumenda animi a suscipit minus dolorem error molestiae, quasi, autem vitae aspernatur odio magnam facilis.",
       number_of_question: 5,
       code: 21,
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,35 @@
 import { getSession } from "next-auth/react"
-import userController from "../controllers/userController"
 import MainHeader from "../components/MainHeader"
+import Link from "next/link"
+import CardView from "../components/CardView"
+import questionController from "../controllers/questionController"
 
-export default function Home(props) {
+export default function Home({data}) {
   return (
     <div className="container py-3">
       <MainHeader />
       <header>
 
-        <main>
-          <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
-            
-          </div>
-
+      <main className="main-bg-color">
+            <div className="row px-3 mt-3">
+              <div className="card border-0 shadow-sm bg-white profile-quize p-5">
+                <h2 className="text-dark">
+                  Would you like to improve your memory and learn more?
+                </h2>
+                <h4 className="pb-5">
+                  The SAPEVI platfor is here for you!
+                  by doing excercises and answering the questions on technical and general knowledge, you can improve your brain and learn many thing. if you are preparing for university entry exams you have a chance to register here and practice with the bank of questionaries. 
+                </h4>
+                <Link href={`/api/auth/signin?callbackUrl=http%3A%2F%2Flocalhost%3A3000%2Fhome`} className = "text-end">
+                  <button className="btn btn-success" >Register Here</button>
+                </Link>
+              </div>
+              {data.map(item=>{
+                return (<CardView title={item.title} desc={item.description} tag={item.title} img={item.imgUrl}/>)
+              })}
+            </div>
         </main>
-      </header>z
+      </header>
     </div>
   )
 
@@ -22,10 +37,9 @@ export default function Home(props) {
 }
 
 export async function getServerSideProps(req, res) {
+  
   const session = await getSession(req)
-  if(session){
-    let currentUser = await userController.findByEmail(session.user)
-    
+  if(session){    
         return {
             redirect: {
             permanent: false,
@@ -34,13 +48,11 @@ export async function getServerSideProps(req, res) {
         }
     
     
-  }else{
-    return {
-        redirect: {
-        permanent: false,
-        destination: `/home`
-        }
-    }
   }
+  const data = await questionController.categories()
+  return {
+    props: {data}
+  }
+
   
 }
